00739c08ffe2827e3b76db543ac63d062f5e8149
Sara Green <smg105c@gmail.com>
2021-04-10 19:49:14 -0700

    initializing project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sierra Summits</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
