a325d792dbfe05c8195f8da3db207de297d7e881
Sara Green <smg105c@gmail.com>
2021-05-02 11:29:21 -0700

    completed homepage responsive styling

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -8,10 +8,10 @@ class Homepage extends Component {
     render() {
         return (
             <div>
-                <img src={mountains} alt="" id="homeImg" />
-                <div className="homepageText">
+                {/* <img src={mountains} alt="" id="homeImg" /> */}
+                <div  className="homepageText">
                     <p>Sierra Summits is an online summit register.</p>
-                    <p>It allows users to access information on the 247 SPS peaks.</p>
+                    <p>It allows users to access information on the 247 SPS peaks throughout the Sierra Nevada mountain range.</p>
                     <p>Users can sort and filter peaks based on milage, elevation or class.</p>
                     <p>After summiting a peak, users can sign the online summit register or leave comments for other users on trail conditions.</p>
                     <Link className='Link' to='/api/peaks'>
